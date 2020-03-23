2530db5c3a317326b6f1eae706e9e77da54c5779
MiaaaaaaZ <mirongzou@gmail.com>
2020-03-23 15:18:17 -0700

    added forkify

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,36 +3,59 @@ import React from 'react';
 const projects = [
   {
     id: 1,
+    title: 'Forkify',
+    git: 'https://github.com/MiaZou/forkify',
+    heroku: 'http://73.71.152.77:667/',
+    img: 'forkify.png',
+    description: 'A recipe search app',
+    language: 'JS, node, webpack'
+  },
+  {
+    id: 2,
     title: 'Nomster',
     git: 'https://github.com/MiaZou/Nomster',
     heroku: 'https://nomster-mia-zou.herokuapp.com/',
     img: 'nomster.jpg',
-    description: 'A Yelp clone'
+    description: 'A Yelp clone',
+    language: 'Ruby, Rails, Amazon S3'
   },
   {
-    id: 2,
+    id: 3,
     title: 'VideosApp',
     git: 'https://github.com/MiaZou/videosApp',
     heroku: 'https://videos-react-app.herokuapp.com/',
     img: 'videosReact.jpg',
-    description: 'A Youtube clone'
+    description: 'A Youtube clone',
+    language: 'JS, React, Youtube API'
   },
   {
-    id: 3,
+    id: 4,
     title: 'Flixter',
     git: 'https://github.com/MiaZou/Flixter',
     heroku: 'https://flixter-mia-zou.herokuapp.com/',
     img: 'flixter.jpg',
-    description: 'A Udemy clone'
+    description: 'A Udemy clone',
+    language: 'Ruby, Rails, Stripe API'
   },
   {
-    id: 4,
+    id: 5,
     title: 'Todoster',
     git: 'https://github.com/MiaZou/todo-mz',
     heroku: 'http://todoster-mia-zou.herokuapp.com/',
     img: 'todoster.jpg',
-    description: 'A to-do list'
-  }
+    description: 'A to-do list',
+    language: 'JavaScript'
+  },
+  {
+    id: 6,
+    title: 'Note',
+    git: 'https://github.com/MiaZou/note-app-mz',
+    heroku: 'https://note-app-mz.herokuapp.com/',
+    img: 'note.png',
+    description: 'A note-taking app',
+    language: 'JavaScript, React'
+  },
+
 ];
 
 
@@ -61,6 +84,9 @@ class Project extends React.Component {
           </div>
           <div className="project-des">
             {project.description}
+          </div>
+          <div className="project-lan">
+            {project.language}
           </div>
           <div align="bottom" className="project-img">
             <img src={project.img} alt="" ></img>
