ad3459213d2ed34e77dd3a2f6a0ce2ed1e63b460
MiaZou <mirongzou@gmail.com>
2020-08-24 21:35:31 -0700

    update bio

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ class About extends React.Component {
           <h2>WEB DEVELOPER / REGISTERED DIETITIAN</h2>
           <hr className="grey"/>
           <p className="bio">
-            I am a self-motivated full-stack developer with a passion for code writing. 
+            I am a self-motivated full-stack developer with a passion for code writing. Currently working as a mobile developer intern for both iOS and Android using Flutter.
             
             Previously worked as a Registered Dietitian I possess enhanced problem-solving skills similar to those required for software development. I can effectively break a big problem into smaller components where I aim to fix one piece at a time efficiently. 
             
